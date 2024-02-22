4c5e7297ad1424d235b9c3bfc29fdd2a9ccd734a
Evan Tao <evantao@Evans-MacBook-Pro-4.local>
2024-02-22 10:54:45 +0100

    complete all model tests

--- src/tests/model_test.go
+++ src/tests/model_test.go
@@ -10,6 +10,11 @@ type modelTest struct {
 	expected int
 }
 
+type itemTest struct {
+	arg []models.Item
+	expected int
+}
+
 var alphaTests = []modelTest{
 	modelTest{"Target", 6},
 	modelTest{"Walgreens", 9},
@@ -30,6 +35,53 @@ var multiple25Tests = []modelTest{
 	modelTest{"0.25", 25},
 	modelTest{"9.00", 25},
 	modelTest{"8.75", 25},
+}
+
+var dayTests = []modelTest{
+	modelTest{"2022-01-02", 0},
+	modelTest{"2022-01-01", 6},
+	modelTest{"2022-03-20", 0},
+	modelTest{"2022-03-17", 6},
+}
+
+var timeTests = []modelTest{
+	modelTest{"13:13", 0},
+	modelTest{"08:13", 0},
+	modelTest{"14:01", 10},
+	modelTest{"15:59", 10},
+	modelTest{"16:00", 0},
+}
+
+var numItemsTests = []itemTest{
+	itemTest{[]models.Item{{"Pepsi - 12-oz", "1.25"}}, 0},
+	itemTest{[]models.Item{{"Pepsi - 12-oz", "1.25"}, 
+						   {"Dasani", "1.40"}}, 5},
+	itemTest{[]models.Item{{"Mountain Dew 12PK", "6.49"}, 
+						   {"Emils Cheese Pizza", "12.25"},
+						   {"Knorr Creamy Chicken", "1.26"},
+						   {"Doritos Nacho Cheese", "3.35"},
+						   {"   Klarbrunn 12-PK 12 FL OZ  ", "12.00"},}, 10},
+	itemTest{[]models.Item{{"Gatorade", "2.25"}, 
+						   {"Gatorade", "2.25"}, 
+						   {"Gatorade", "2.25"}, 
+						   {"Gatorade", "2.25"}, 
+						   {"Gatorade", "2.25"}, }, 10},
+}
+
+var descriptionTests = []itemTest{
+	itemTest{[]models.Item{{"Pepsi - 12-oz", "1.25"}}, 0},
+	itemTest{[]models.Item{{"Pepsi - 12-oz", "1.25"}, 
+						   {"Dasani", "1.40"}}, 1},
+	itemTest{[]models.Item{{"Mountain Dew 12PK", "6.49"}, 
+						   {"Emils Cheese Pizza", "12.25"},
+						   {"Knorr Creamy Chicken", "1.26"},
+						   {"Doritos Nacho Cheese", "3.35"},
+						   {"   Klarbrunn 12-PK 12 FL OZ  ", "12.00"},}, 6},
+	itemTest{[]models.Item{{"Gatorade", "2.25"}, 
+						   {"Gatorade", "2.25"}, 
+						   {"Gatorade", "2.25"}, 
+						   {"Gatorade", "2.25"}, 
+						   {"Gatorade", "2.25"}, }, 0},
 }
 
 func TestAlpha(t *testing.T){
@@ -56,54 +108,34 @@ func TestMultiple25(t *testing.T){
 	}
 }
 
-func TestNumItems(t *testing.T){
-
-	items := []models.Item{
-		{"Pepsi - 12-oz", "1.25"},
-	}
-
-	got := models.GetNumItemsPoints(items)
-	want := 0
-
-	if got != want {
-		t.Errorf("got %d, wanted %d", got, want)
-	}
-}
-
 func TestDay(t *testing.T){
-
-	purchaseDate := "2022-01-02"
-
-	got := models.GetDayPoints(purchaseDate)
-	want := 0
-
-	if got != want {
-		t.Errorf("got %d, wanted %d", got, want)
+	for _, test := range dayTests{
+		if output := models.GetDayPoints(test.arg); output != test.expected {
+			t.Errorf("Output %d not equal to expected %d", output, test.expected)
+		}
 	}
 }
 
 func TestTime(t *testing.T){
+	for _, test := range timeTests{
+		if output := models.GetTimePoints(test.arg); output != test.expected {
+			t.Errorf("Output %d not equal to expected %d", output, test.expected)
+		}
+	}
+}
 
-	purchaseTime := "13:13"
-
-	got := models.GetTimePoints(purchaseTime)
-	want := 0
-
-	if got != want {
-		t.Errorf("got %d, wanted %d", got, want)
+func TestNumItems(t *testing.T){
+	for _, test := range numItemsTests{
+		if output := models.GetNumItemsPoints(test.arg); output != test.expected {
+			t.Errorf("Output %d not equal to expected %d", output, test.expected)
+		}
 	}
 }
 
 func TestDescription(t *testing.T){
-
-	items := []models.Item{
-		{"Pepsi - 12-oz", "1.25"},
-	}
-
-	got := models.GetDescriptionPoints(items)
-	want := 0
-
-	if got != want {
-		t.Errorf("got %d, wanted %d", got, want)
+	for _, test := range descriptionTests{
+		if output := models.GetDescriptionPoints(test.arg); output != test.expected {
+			t.Errorf("Output %d not equal to expected %d", output, test.expected)
+		}
 	}
 }
