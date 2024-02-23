5f0528042e9518ac72dfa7b06b1a4f1bd4edc4c8
Evan Tao <evantao@Evans-MacBook-Pro-4.local>
2024-02-23 18:12:06 +0100

    added check for id in test processreceipt

--- src/tests/feature_test.go
+++ src/tests/feature_test.go
@@ -2,11 +2,11 @@ package tests
 
 import (
 	"bytes"
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"src/controllers"
 	"testing"
 )
@@ -27,8 +27,8 @@ type getPointsTest struct {
 
 // Tests for the expected HTTP code and body based on the receipt file
 var processReceiptTests = []processReceiptTest {
-	processReceiptTest{"json/test1.json", 200, `{"id":"^\\S+$"}`},
-	processReceiptTest{"json/test2.json", 200, `{"id":"^\\S+$"}`},
+	processReceiptTest{"json/test1.json", 200, `{"id":`},
+	processReceiptTest{"json/test2.json", 200, `{"id":`},
 	processReceiptTest{"json/test3.json", 400, "The receipt is invalid"},
 	processReceiptTest{"json/test4.json", 400, "The receipt is invalid"},
 	processReceiptTest{"json/test5.json", 400, "The receipt is invalid"},
@@ -49,13 +49,12 @@ var processReceiptTests = []processReceiptTest {
 var getPointsTests = []getPointsTest {
 	getPointsTest{"", 404, "No receipt found for that id"},
 	getPointsTest{"abc", 404, "No receipt found for that id"},
-}
 
-var id1 = ""
-var id2 = ""
+}
 
 // Iterates through tests and prints the results
 func TestProcessReceipt(t *testing.T) {
+	gin.SetMode(gin.ReleaseMode)
     r := gin.Default()
     r.POST("/receipts/process", controllers.ProcessReceipt)
     for _,test := range processReceiptTests{
@@ -67,18 +66,14 @@ func TestProcessReceipt(t *testing.T) {
  		if outputCode := w.Code; outputCode != test.expectedCode {
  			t.Errorf("Output code %d not equal to expected code %d", outputCode, test.expectedCode)
  		}
- 		if outputBody := w.Body.String(); outputBody != test.expectedBody {
+ 		if outputBody := w.Body.String(); strings.Index(outputBody, test.expectedBody) < 0 {
 			t.Errorf(`Output body "%s" not equal to expected body "%s"`, outputBody, test.expectedBody)
- 		}
-		if test.arg == "json/test1.json" {
-			outputBody := w.Body.String(); id1 = outputBody 
-			fmt.Println(id1)
  		}
     }
 }
 
 func TestGetPoints(t *testing.T) {
-    r := gin.Default()
+	r := gin.Default()
 	r.GET("/receipts/:id/points", controllers.GetPoints)
     for _,test := range getPointsTests{
     	w := httptest.NewRecorder()
