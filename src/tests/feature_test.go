944be2f604a58a58731947636fc3d1f916993b6d
Evan Tao <evantao@Evans-MacBook-Pro-4.local>
2024-02-23 13:18:22 +0100

    added all feature tests

--- src/tests/feature_test.go
+++ src/tests/feature_test.go
@@ -8,17 +8,47 @@ import (
 	"bytes"
 	"src/controllers"
 	"github.com/gin-gonic/gin"
+	"io/ioutil"
 )
 
+type processTest struct {
+	arg string
+	expectedCode int
+	expectedMsg string
+}
+
+var processTests = []processTest{
+	processTest{"json/test1.json", 200, `{"id":"The receipt is invalid"}`},
+	processTest{"json/test2.json", 200, `{"id":"The receipt is invalid"}`},
+	processTest{"json/test3.json", 400, "The receipt is invalid"},
+	processTest{"json/test4.json", 400, "The receipt is invalid"},
+	processTest{"json/test5.json", 400, "The receipt is invalid"},
+	processTest{"json/test6.json", 400, "The receipt is invalid"},
+	processTest{"json/test7.json", 400, "The receipt is invalid"},
+	processTest{"json/test8.json", 400, "The receipt is invalid"},
+	processTest{"json/test9.json", 400, "The receipt is invalid"},
+	processTest{"json/test10.json", 400, "The receipt is invalid"},
+	processTest{"json/test11.json", 400, "The receipt is invalid"},
+	processTest{"json/test12.json", 400, "The receipt is invalid"},
+	processTest{"json/test13.json", 400, "The receipt is invalid"},
+	processTest{"json/test14.json", 400, "The receipt is invalid"},
+	processTest{"json/test15.json", 400, "The receipt is invalid"},
+	processTest{"json/test16.json", 400, "The receipt is invalid"},
+	processTest{"json/test17.json", 400, "The receipt is invalid"},
+}
+
 func TestProcessReceipt(t *testing.T) {
-	jsonBody := []byte(`{"retailer": "Target"}`)
- 	bodyReader := bytes.NewReader(jsonBody)
     r := gin.Default()
     r.POST("/receipts/process", controllers.ProcessReceipt)
-	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("POST", "/receipts/process", bodyReader)
-	r.ServeHTTP(w, req)
-
-	assert.Equal(t, 400, w.Code)
-	assert.Equal(t, `{"status":"The receipt is invalid"}`, w.Body.String())
+    for _,test := range processTests{
+    	w := httptest.NewRecorder()
+    	file, _ := ioutil.ReadFile(test.arg)
+ 		reader := bytes.NewReader(file)
+ 		req, _ := http.NewRequest("POST", "/receipts/process", reader)
+ 		r.ServeHTTP(w, req)
+		assert.Equal(t, test.expectedCode, w.Code)
+		assert.Equal(t, test.expectedMsg, w.Body.String())
+    }
 }
+
+
